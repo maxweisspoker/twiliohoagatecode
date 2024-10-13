d170d7a79ce3cc9ad1db98f93f55c13acce8b9d8
Maximilian Weiss <max@maxweiss.io>
2024-10-12 21:34:38 -0700

    fix and add voice record

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/", doRespond)
+	r.GET("/recordvoice", doRespond2)
 	_ = r.Run(":8080")
 }
 
@@ -17,9 +18,21 @@ func doRespond(c *gin.Context) {
 	resp := `
 <?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Play digits="wwww9wwwwwwww"/>
+    <Play digits="wwwwwwwwwwww9ww9ww9wwwwwwwwwwwwwwwwwwwwwwww"/>
     <Hangup/>
 </Response>
 	`
 	c.Data(200, "text/xml", []byte(strings.TrimSpace(resp)+"\n"))
 }
+
+func doRespond2(c *gin.Context) {
+	resp := `
+<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Record timeout="30" transcribe="false" />
+    <Hangup/>
+</Response>
+	`
+	c.Data(200, "text/xml", []byte(strings.TrimSpace(resp)+"\n"))
+}
+
